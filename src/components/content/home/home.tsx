303040c43ce434c9620bda7e8277645c5cb2eae7
Wouter van Heeswijk <woutervh@gmail.com>
2018-05-14 20:48:55 +0200

    Added copyright

--- src/components/content/home/home.tsx
+++ src/components/content/home/home.tsx
@@ -3,40 +3,40 @@ import * as React from 'react';
 export class Home extends React.Component<{}, never> {
     render() {
         return <React.Fragment>
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
-            Home! This is the bibauw website LOLOLOL lion. rR<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
+            Home! This is the ultra deluxe.<br />
         </React.Fragment>;
     }
 }

66cbfa8914960273cbddedf7e973f5d246bfea4a
Wouter van Heeswijk <woutervh@gmail.com>
2018-11-10 16:30:39 +0100

    Put back external scripts

--- webpack.prod.static.js
+++ webpack.prod.static.js
@@ -3,7 +3,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.prod.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackStaticPlugin = require('./html-webpack-static-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpackHash = require('./webpack.hash');
 
 const additionalTags = [{
@@ -59,12 +58,12 @@ module.exports = merge(common, {
         }]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', location: '/', filename: 'index.html' }),
-        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', location: '/author', filename: 'author/index.html' }),
-        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', location: '/contact', filename: 'contact/index.html' }),
-        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', location: '/contact-thanks', filename: 'contact-thanks/index.html' }),
-        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', location: '/resources', filename: 'resources/index.html' }),
-        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', location: '/blog', filename: 'blog/index.html' }),
+        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', additionalTags, location: '/', filename: 'index.html' }),
+        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', additionalTags, location: '/author', filename: 'author/index.html' }),
+        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', additionalTags, location: '/contact', filename: 'contact/index.html' }),
+        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', additionalTags, location: '/contact-thanks', filename: 'contact-thanks/index.html' }),
+        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', additionalTags, location: '/resources', filename: 'resources/index.html' }),
+        new HtmlWebpackPlugin({ template: './src/index.html', chunks: ['bundle'], entry: 'static', additionalTags, location: '/blog', filename: 'blog/index.html' }),
         new HtmlWebpackStaticPlugin()
     ]
 });

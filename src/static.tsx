05cb5087933e07dd5ecf67f7875c8b9b5f81d854
Wouter van Heeswijk <woutervh@gmail.com>
2018-12-08 20:31:39 +0100

    Fix import filename

--- src/static.tsx
+++ src/static.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
-import { App } from './App';
+import { App } from './app';
 import * as ReactRouterDOM from 'react-router-dom'
 
 export default (location: string) => ReactDOMServer.renderToString(

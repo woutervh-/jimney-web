d3a26bd538e5f6408a4a55e9daf55ba1851e8bab
Wouter van Heeswijk <woutervh@gmail.com>
2022-05-11 17:31:31 +0200

    Use plain html for mailchimp form

---
+++ src/components/side-bar/mail-chimp-form.ts
@@ -0,0 +1,47 @@
+export const mailChimpForm = `
+<div id="mc_embed_signup">
+    <form
+        action="https://jasmingelinck.us14.list-manage.com/subscribe/post?u=a8c9b88db9ffc1f70ec6ac413&amp;id=5fd6713355"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        class="validate"
+        target="_blank"
+        novalidate
+    >
+        <div id="mc_embed_signup_scroll">
+            <h2>Join my Mailing List!</h2>
+            <div class="indicates-required">
+                <span class="asterisk">*</span> indicates required
+            </div>
+            <div class="mc-field-group">
+                <label for="mce-EMAIL">
+                    Email Address <span class="asterisk">*</span>
+                </label>
+                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" />
+            </div>
+            <div class="mc-field-group">
+                <label for="mce-FNAME">First Name </label>
+                <input type="text" value="" name="FNAME" class="" id="mce-FNAME" />
+            </div>
+            <div id="mce-responses" class="clear foot">
+                <div class="response" id="mce-error-response" style="display: none"></div>
+                <div class="response" id="mce-success-response" style="display: none"></div>
+            </div>
+            <div style="position: absolute; left: -5000px" aria-hidden="true">
+                <input type="text" name="b_a8c9b88db9ffc1f70ec6ac413_5fd6713355" tabIndex="-1" value="" />
+            </div>
+            <div class="optionalParent">
+                <div class="clear">
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
+                    <p class="brandingLogo">
+                        <a href="http://eepurl.com/h1yLo9" title="Mailchimp - email marketing made easy and fun">
+                            <img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg" />
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+`;
--- src/components/side-bar/side-bar.tsx
+++ src/components/side-bar/side-bar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { mailChimpForm } from './mail-chimp-form';
 
 export class SideBar extends React.PureComponent<{}, never> {
     render() {
@@ -18,52 +19,7 @@ export class SideBar extends React.PureComponent<{}, never> {
                     <img alt="" width="1" height="1"
                         src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" />
                 </form>
-            </div>
-            <div id="mc_embed_signup">
-                <form
-                    action="https://jasmingelinck.us14.list-manage.com/subscribe/post?u=a8c9b88db9ffc1f70ec6ac413&amp;id=5fd6713355"
-                    method="post" id="mc-embedded-subscribe-form"
-                    name="mc-embedded-subscribe-form"
-                    className="validate"
-                    target="_blank"
-                    noValidate
-                >
-                    <div id="mc_embed_signup_scroll">
-                        <h2>Join my Mailing List!</h2>
-                        <div className="indicates-required">
-                            <span className="asterisk">*</span>
-                            indicates required
-                        </div>
-                        <div className="mc-field-group">
-                            <label htmlFor="mce-EMAIL">
-                                Email Address
-                                <span className="asterisk">*</span>
-                            </label>
-                            <input type="email" value="" name="EMAIL" className="required email" id="mce-EMAIL" />
-                        </div>
-                        <div className="mc-field-group">
-                            <label htmlFor="mce-FNAME">First Name </label>
-                            <input type="text" value="" name="FNAME" className="" id="mce-FNAME" />
-                        </div>
-                        <div id="mce-responses" className="clear foot">
-                            <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
-                            <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
-                        </div>
-                        <div style={{ position: 'absolute', left: -5000 }} aria-hidden="true">
-                            <input type="text" name="b_a8c9b88db9ffc1f70ec6ac413_5fd6713355" tabIndex={-1} value="" />
-                        </div>
-                        <div className="optionalParent">
-                            <div className="clear">
-                                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
-                                <p className="brandingLogo">
-                                    <a href="http://eepurl.com/h1yLo9" title="Mailchimp - email marketing made easy and fun">
-                                        <img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg" />
-                                    </a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                <div dangerouslySetInnerHTML={{ __html: mailChimpForm }} />
             </div>
         </React.Fragment>;
     }
